f9be7082a9074a569fc7b1bd845453b8b57d1155
Naim Chebbi <nachebbi@c1r2s1.42lausanne.ch>
2025-07-07 17:31:20 +0200

    understand ft_putnbr_base

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -12,86 +12,157 @@
 
 #include <unistd.h>
 
-int	ft_is_alpha(char *str)
+void    ft_putchar(char c)
 {
-	int	i;
+    write(1, &c, 1);
+}
 
+int base_check(char *base)
+{
+    int len;
+	int i;
+	int j;
+
+	j = 0;
 	i = 0;
-	while (str[i])
-	{
-		if (!((str[i] >= 'a' && str[i] <= 'z')
-				|| (str[i] >= 'A' && str[i] <= 'Z')
-				|| (str[i] >= '0' && str[i] <= '9')))
-			return (0);
-		i++;
-	}
-	return (1);
+    len = 0;
+    while (base[len])
+        len++;
+    if (len <= 1)
+        return (0);
+    while (base[i])
+    {
+        j = i + 1;
+        while (base[j])
+        {
+            if (base[i] == '-' || base[i] == '+' || base[i] == base[j])
+                return (0);
+            j++;
+        }
+        i++;
+    }
+    return (len);
 }
 
-int	ft_strlen(char *str)
+void    ft_putnbr_base(int nbr, char *base)
 {
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
+    int len;
+    
+    len = base_check(base);
+    if (len <= 0)
+        return ;
+    if (nbr < 0)
+    {
+        nbr *= -1;
+        ft_putchar('-');
+    }
+    if (nbr + 1 <= len)
+    {
+        ft_putchar(base[nbr]);
+    }
+    else if (nbr >= len)
+    {
+        ft_putnbr_base(nbr / len, base);
+        ft_putnbr_base(nbr % len, base);
+    }
 }
 
-int	check_dup(char *base)
-{
-	int	i;
-	int	j;
+// #include <unistd.h>
 
-	i = 0;
-	if (ft_strlen(base) <= 1)
-		return (0);
-	while (base[i])
-	{
-		j = i + 1;
-		while (base [j])
-		{
-			if (base[i] == base[j])
-				return (1);
-			j++;
-		}
-		i++;
-	}
-	return (0);
-}
+// int	ft_is_alpha(char *str)
+// {
+// 	int	i;
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+// 	i = 0;
+// 	while (str[i])
+// 	{
+// 		if (!((str[i] >= 'a' && str[i] <= 'z')
+// 				|| (str[i] >= 'A' && str[i] <= 'Z')
+// 				|| (str[i] >= '0' && str[i] <= '9')))
+// 			return (0);
+// 		i++;
+// 	}
+// 	return (1);
+// }
 
-void	ft_putnbr_base(int nbr, char *base)
-{
-	long	number ;
-	int		base_len;
+// int	ft_strlen(char *str)
+// {
+// 	int	i;
 
-	base_len = ft_strlen(base);
-	number = nbr;
-	if (base_len <= 1 || !ft_is_alpha(base) || check_dup(base) == 1)
-		return ;
-	if (number < 0)
-	{
-		ft_putchar('-');
-		number *= -1;
-	}
-	if (nbr < base_len)
-		ft_putchar(base[nbr]);
-	else
-	{
-		ft_putnbr_base(nbr / base_len, base);
-		ft_putnbr_base(nbr % base_len, base);
-	}
-}
+// 	i = 0;
+// 	while (str[i])
+// 		i++;
+// 	return (i);
+// }
+
+// int	check_dup(char *base)
+// {
+// 	int	i;
+// 	int	j;
+
+// 	i = 0;
+// 	if (ft_strlen(base) <= 1)
+// 		return (0);
+// 	while (base[i])
+// 	{
+// 		j = i + 1;
+// 		while (base [j])
+// 		{
+// 			if (base[i] == base[j])
+// 				return (1);
+// 			j++;
+// 		}
+// 		i++;
+// 	}
+// 	return (0);
+// }
+
+// void	ft_putchar(char c)
+// {
+// 	write(1, &c, 1);
+// }
+
+// void	ft_putnbr_base(int nbr, char *base)
+// {
+// 	long	number ;
+// 	int		base_len;
+
+// 	base_len = ft_strlen(base);
+// 	number = nbr;
+// 	if (base_len <= 1 || !ft_is_alpha(base) || check_dup(base) == 1)
+// 		return ;
+// 	if (number < 0)
+// 	{
+// 		ft_putchar('-');
+// 		number *= -1;
+// 	}
+// 	if (nbr < base_len)
+// 		ft_putchar(base[nbr]);
+// 	else
+// 	{
+// 		ft_putnbr_base(nbr / base_len, base);
+// 		ft_putnbr_base(nbr % base_len, base);
+// 	}
+// }
 
 int	main(void)
 {
 	int	nbr = -23453453;
+	int	nbr1 = 23453453;
+	int	nbr2 = 23453453;
+	int	nbr3 = -23453453;
 	char base[] = "0123456789ABCDEF";
+	char base1[] = "ABCDEF";
+	char base2[] = "0123456789";
+	char base3[] = "01";
 	ft_putnbr_base(nbr, base);
+	ft_putchar('\n');
+	ft_putnbr_base(nbr1, base1);
+	ft_putchar('\n');
+	ft_putnbr_base(nbr2, base3);
+	ft_putchar('\n');
+	ft_putnbr_base(nbr3, base2);
+	ft_putchar('\n');
+	ft_putnbr_base(nbr3, base3);
 	return (0);
 }
